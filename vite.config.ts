0d8c5a5e07a7e894a3ccdeaac0269b499178e96d
José Eduardo Ormeño Meneses <eduardoorm79@gmail.com>
2024-12-18 18:03:22 -0500

    base 4

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/web-computeup/', // Reemplaza 'nombre-del-repositorio' con el nombre de tu repositorio
+  base: '/web-computeup/', 
   plugins: [react()],
   build: {
     outDir: 'dist',
